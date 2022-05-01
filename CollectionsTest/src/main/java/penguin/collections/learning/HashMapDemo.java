15930ad766588fbeb7ee5af4931f471d562e2c62
vPENGuin <vp3rustemfdmairv@gmail.com>
2022-05-01 22:29:59 +0300

    Add book of names

--- CollectionsTest/src/main/java/penguin/collections/learning/HashMapDemo.java
+++ CollectionsTest/src/main/java/penguin/collections/learning/HashMapDemo.java
@@ -1,16 +1,29 @@
 package penguin.collections.learning;
 
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-        Map<Character, List<String>> nameList = new HashMap<>();
+        Map<Character, Set<String>> bookOfNames = new HashMap<>();
 
-        String[] namesArray = {"Марина", "Мария", "Анастасия", "Денис", "Артемий", "Кира", "Таисия", "Лев",
-                "Тихон", "София", "Ульяна", "Адам", "Майя", "Варвара", "Нина", "Елизавета", "Михаил", "Ростислав", "Дмитрий"};
+        String[] namesArray = {"Марина", "мария", "Мария", "Анастасия", "Денис", "Артемий", "Кира", "Таисия", "Лев",
+                "Тихон", "София", "Ульяна", "Адам", "Майя", "Варвара", "Нина", "мария", "Елизавета", "Михаил", "Ростислав",
+                "Дмитрий", "Дмитрий", "Мария"};
 
-        //todo : доделатб
+        for (String name : namesArray) {
+            Character firstLetter = name.toUpperCase().charAt(0);
+            if (!bookOfNames.containsKey(firstLetter)) {
+                Set<String> value = new HashSet<>();
+                value.add(name);
+                bookOfNames.put(firstLetter, value);
+            } else {
+                Set<String> value = bookOfNames.get(firstLetter);
+                value.add(name);
+            }
+        }
+        System.out.println(bookOfNames);
     }
 }

3a31134f28b6abd14e236342e7acf2447639cf4f
morjokill <morjokill@gmail.com>
2022-05-15 20:54:09 +0300

    Add string builder demo problem

---
+++ strings-learning/src/main/java/strings/learning/builder/StringBuilderDemo.java
@@ -0,0 +1,11 @@
+package strings.learning.builder;
+
+public class StringBuilderDemo {
+    public static void main(String[] args) {
+        // String[] strings = [1000];
+
+        // "asdasdasd", "asdasfasf", "asfdasgfjakl"
+
+        // "asdasdasdasdasfasfasfdasgfjakl"
+    }
+}
